2f93f4bc4038b4f2d8da655730cb688bf1df93e2
Ethan Low <ethanlow223@yahoo.com>
2018-12-01 00:04:36 -0800

    initial commit

---
+++ js/agency.js
@@ -0,0 +1,68 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$('div.modal').on('show.bs.modal', function() {
+	var modal = this;
+	var hash = modal.id;
+	window.location.hash = hash;
+	window.onhashchange = function() {
+		if (!location.hash){
+			$(modal).modal('hide');
+		}
+	}
+});
+
+// Modal Gallery
+var slideIndex;
+function modalImages(id) {
+  slideIndex = 1;
+  showSlides(slideIndex, id);
+}
+// Next/previous controls
+function plusSlides(n, id) {
+  showSlides(slideIndex += n, id);
+}
+// Thumbnail image controls
+function currentSlide(n, id) {
+  showSlides(slideIndex = n, id);
+}
+
+function showSlides(n, id) {
+  var i;
+  var slides = document.getElementsByClassName("slides" + id);
+  var dots = document.getElementsByClassName("dots" + id);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
